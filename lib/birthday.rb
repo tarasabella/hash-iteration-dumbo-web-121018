f963e37b0cd58e4a74ab40985b664a0e0a5083e5
Tara Sabella <tarasabella@gmail.com>
2018-12-03 00:11:14 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,8 +6,10 @@
 
 
  def happy_birthday(birthday_kids)
+   # define method to take in the birthday_kids hash as an argument 
   birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    #iterate through each kid in the hash to puts a birthday message with their age
   end
 end
 
--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,10 +7,13 @@
 # }
 
 def select_winner(passengers)
+  # define a method that takes in an argument of the passengers hash 
 winner = ""
 passengers.each do |suite, name|
   if suite == :suite_a && name.start_with?("A")
+#iterate over all passengers and grab the passenger in suite a with name starting with "A"
     winner = name
+    #set winner's name equal to variable to be returned later 
   end
 end
  
