e14bfe2ffd2a532b177785f57c2e9e030b987bda
Tara Sabella <tarasabella@gmail.com>
2018-12-03 00:54:20 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,10 +7,37 @@
 # }
 
 def select_winner(passengers)
-winner = ""
+  winner = ""
 passengers.each do |suite, name|
   if suite == :suite_a && name.start_with?("A")
     winner = name
+  end
+ end 
+winner 
+end 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def select_winner(passengers)
+  # define a method that takes in an argument of the passengers hash 
+winner = ""
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+#iterate over all passengers and grab the passenger in suite a with name starting with "A"
+    winner = name
+    #set winner's name equal to variable to be returned later 
   end
 end
  
